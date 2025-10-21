601caf9953b154396ec8d792f9babe1982231972
Nyi Nyi Aung <nyia7504@gmail.com>
2025-10-21 10:43:55 +0630

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/articles', function () {
+    return 'Articles List';
 });
+
+Route::get('/articles/detail', function () {
+    return 'Article Detail';
+});
+
+
